1adfbf7f31d868f2aa9704227b7fee4beb562774
Derek Mason <3derekmason@gmail.com>
2021-12-09 15:56:03 -0700

    ProfileView pseudocode

--- client/src/components/Profile/ProfileView.jsx
+++ client/src/components/Profile/ProfileView.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Container, Card, Paper } from "@material-ui/core";
 
+import ContractorProfile from "./ContractorProfile.jsx";
+import ClientProfile from "./ClientProfile.jsx";
+
 const ProfileView = () => {
   const [isContractor, setIsContractor] = useState(false);
-
+  // On render setIsContractor based on current user
+  useEffect(() => {
+    //  setIsContractor(currentUser.isContractor)
+  }, []); //  <--- Current user will be watched in here
   return isContractor ? <ContractorProfile /> : <ClientProfile />;
 };
 
