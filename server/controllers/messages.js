685158a413774ab5ec51b751149926e714509da5
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2021-12-10 13:24:50 -0700

    get messages done

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -2,9 +2,16 @@ const pool = require('../db');
 
 module.exports = {
   getMessages: (req, res) => {
-    const userId = Number(req.params.user_id);
-    console.log(userId);
-    const sql = `SELECT * FROM messages WHERE to_id = $1;`;
+    const userId = req.params.user_id;
+    const sql = `SELECT
+      messages.id,
+      users.firstName from_firstName,
+      users.lastName from_lastName,
+      messages.body,
+      messages.date
+    FROM messages INNER JOIN users
+    ON users.id = messages.from_id
+    WHERE to_id = $1;`;
     const values = [userId];
 
     pool.query(sql, values)
