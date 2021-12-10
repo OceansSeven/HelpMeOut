15bc4dc7421f0d0b0595d3980cdd37bcbf09309f
N8RB8 <nschroeder95@gmail.com>
2021-12-09 19:44:34 -0700

    added jobcard component and roughed out main

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Paper } from "@material-ui/core"
+
+function JobCard({data}) {
+  //
+  return (
+    <Paper>
+      <h3> {data.title}</h3>
+      <h4> {data.firstName} {data.lastName[0]}.</h4>
+      <p>Category: {data.specialties.map((category, i) => <span key={i}>{category}{i===data.type.length - 1 ? '' : ', '}</span>)}</p>
+      <div>
+        {data.description}
+      </div>
+      <p>{data.date}</p>
+    </Paper>
+  );
+}
+
+export default JobCard;
