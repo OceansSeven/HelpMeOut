b5d13dce23468af7d7d246ae22f89d2170ba4d4f
Matthew Dowell <mdowell723@gmail.com>
2021-12-09 20:17:57 -0700

    changed titles to login and register

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -55,7 +55,7 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Login
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
