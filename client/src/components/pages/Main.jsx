3983e98f87dc5e1c55908421d31163e6d8967e99
N8RB8 <nschroeder95@gmail.com>
2021-12-11 12:37:38 -0700

    initialized toggle buttons

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -18,6 +18,12 @@ const Main = function Main() {
   const [jobsAvailable, setJobsAvailable] = useState([]);
   const [jobsAccepted, setJobsAccepted] = useState([]);
 
+  //current userfeed -> defaults to client view
+  //current searchFeed ->
+
+
+  const userBtns = (<div><button>Posted</button> <button>Accepted</button></div>)
+
   //get jobs posted by user from API
   useEffect(() => {
     getUser(user.id).then((results) => {
@@ -33,13 +39,16 @@ const Main = function Main() {
   return (
     <div>
       <div className='userPosts'>
+        {user.contractor ? userBtns : null}
+        {/*currentfeed === false ? jobsPosted : jobs accepted */}
         <ListManager data={jobsPosted}>
           <JobPostedCard />
         </ListManager>
-        {/* <ListManager data={jobsAccepted}>
-          <JobCard />
-        </ListManager> */}
+        <ListManager data={jobsAccepted}>
+          <JobPostedCard />
+        </ListManager>
       </div>
+
       <div className='searchList'>
         <ListManager data={contractorList}>
           <Contractors />
