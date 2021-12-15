fa5eede45af4e159324303b1dc80d8cee739c3ee
Charles Tarika <MaxTarika@Charless-MacBook-Pro-3.local>
2021-12-15 17:12:15 -0500

    minor edits

--- client/src/components/ContractorCard.jsx
+++ client/src/components/ContractorCard.jsx
@@ -10,7 +10,9 @@ export default function ContractorCard({data}) {
         <Card className="contractor-card" variant="outlined">
           <Grid container spacing={2}>
             <Grid item xs={8}>
-              <div id="contractorname"><strong>{data.firstname}{' '}{data.lastname}</strong></div>
+              <Link to={`/profile/${data.id}`} style={{ textDecoration: 'none' }}>
+                <div id="contractorname"><strong>{data.firstname}{' '}{data.lastname}</strong></div>
+              </Link>
               {data.company && <div id="contractorcompany" >{data.company}</div>}
             </Grid>
             <Grid item xs={4} id="contractorrating" style={{ display: 'flex', justifyContent: 'flex-end' }}>
